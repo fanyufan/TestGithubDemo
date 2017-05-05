eed7aeb6d0550f9a3bf108344f0cc949bb259899
john <john@simonlaodemac-mini.local>
2017-05-05 11:35:31 +0800

    初始化Person类

---
+++ TestDemo/TestDemo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  TestDemo
+//
+//  Created by john on 2017/5/5.
+//  Copyright © 2017年 Sam Fan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ TestDemo/TestDemo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  TestDemo
+//
+//  Created by john on 2017/5/5.
+//  Copyright © 2017年 Sam Fan. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
